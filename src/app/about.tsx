4edf68301f7a552df2482ae30dc9c40811f24289
VOWAC <yukihakanai.2@gmail.com>
2024-02-24 12:45:11 +0900

    add about

---
+++ src/app/about.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <Image src="/fydnr.png" width={1000} height={1000} alt="profile" />
+    </div>
+  )
+}
+
+export default About
