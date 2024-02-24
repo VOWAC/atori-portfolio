468c2e3b8d8ef19773d5fce38d6531ff36975bf0
VOWAC <yukihakanai.2@gmail.com>
2024-02-24 12:43:20 +0900

    fix header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-5 border-b-[1px] border-white transition-colors duration-300 text-2xl top-0 left-0 w-full z-50 fixed backdrop-blur-md hover:bg-white hover:bg-opacity-10">
+    <div className="flex justify-between items-center p-5 border-b-[1px] border-white transition-colors duration-300 text-2xl top-0 left-0 w-full z-50 fixed backdrop-blur-sm hover:bg-white hover:bg-opacity-10">
       冬隣創造処
     </div>
   );
