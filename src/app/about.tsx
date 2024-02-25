d74aac2a0e7089090c40a368b359f70e39610594
VOWAC <yukihakanai.2@gmail.com>
2024-02-25 09:13:12 +0900

    add circle

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -10,6 +10,7 @@ const About = () => {
         <Title title="自己紹介" />
         <Introduction />
       </div>
+      <div className='w-[780px] h-[780px] border-[1px] rounded-full fixed top-40 right-20 -z-50'/>
       <Image src="/fydnr.png" width={1000} height={1000} alt="profile" />
     </div>
   );
