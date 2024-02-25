d3668ce85f608116f070bd536a3a8021e20dbfdc
VOWAC <yukihakanai.2@gmail.com>
2024-02-25 15:21:56 +0900

    Adjusting the background css

--- src/app/Illustration/page.tsx
+++ src/app/Illustration/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <div className='border-[1px] border-white w-full fixed top-1/2' />
+    </div>
   )
 }
 
