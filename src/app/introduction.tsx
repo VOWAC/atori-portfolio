2d2bf34d54938300ebd5d4d7196292e57c8b599b
VOWAC <yukihakanai.2@gmail.com>
2024-02-25 09:06:18 +0900

    add introduction

---
+++ src/app/introduction.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <div className='border-y-[1px] p-10'>
+      <p>
+      冬隣あとりです。ふゆどなり、と読みます。
+        <br />
+        <br />
+        あらゆる表現、創作が好きで、言葉、イラスト、歌詞制作、
+        <br />
+        動画など様々なものに手を出しています。
+        <br />
+        キャラクターデザインも好きで、オリジナルキャラクター中心に
+        <br />
+        創作をすることが多いです。
+        <br />
+        全ての分野においてまだ未熟で趣味程度でしかないですが、
+        <br />
+        楽しく創作を続けられたらなと思います。
+      </p>
+    </div>
+  )
+}
+
+export default Introduction
