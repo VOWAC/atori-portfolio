95b49c9996419769dc792e1758a456b4ea1a956f
VOWAC <yukihakanai.2@gmail.com>
2024-02-25 09:06:07 +0900

    add title

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,12 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import Title from './title';
+import Introduction from './introduction';
 
 const About = () => {
   return (
-    <div>
+    <div className='flex'>
+      <div>
+        <Title title="自己紹介" />
+        <Introduction />
+      </div>
       <Image src="/fydnr.png" width={1000} height={1000} alt="profile" />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ src/app/title.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+  title: string;
+};
+
+const Title = ({ title }: Props) => {
+  return (
+    <div className='mt-64 flex space-x-3 items-center'>
+      <div className='bg-white w-3 h-3' />
+      <h1 className='text-2xl'>{title}</h1>
+    </div>
+  );
+};
+
+export default Title;
