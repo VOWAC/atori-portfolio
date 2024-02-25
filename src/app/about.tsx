58f5562366fb6e37609c17aaeb61636c6bafc26b
VOWAC <yukihakanai.2@gmail.com>
2024-02-25 15:01:13 +0900

    fix css

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -5,13 +5,17 @@ import Introduction from './introduction';
 
 const About = () => {
   return (
-    <div className='flex'>
+    <div className="flex items-center">
       <div>
         <Title title="自己紹介" />
+        <div className='absolute top-1/4'>
         <Introduction />
+        </div>
       </div>
-      <div className='w-[780px] h-[780px] border-[1px] rounded-full fixed top-40 right-20 -z-50'/>
-      <Image src="/fydnr.png" width={1000} height={1000} alt="profile" />
+      <div className="w-[780px] h-[780px] border-[1px] rounded-full fixed top-40 right-20 -z-50" />
+      <div className='absolute top-[7%] -right-1/4'>
+      <Image src="/fydnr.png" width={1700} height={3000} alt="profile" />
+      </div>
     </div>
   );
 };
--- src/app/title.tsx
+++ src/app/title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Title = ({ title }: Props) => {
   return (
-    <div className='mt-64 flex space-x-3 items-center'>
+    <div className='absolute top-52 flex space-x-3 items-center'>
       <div className='bg-white w-3 h-3' />
       <h1 className='text-2xl'>{title}</h1>
     </div>
