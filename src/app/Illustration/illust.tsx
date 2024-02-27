b00d4f262dc804ff7847a83e873941975c954004
VOWAC <yukihakanai.2@gmail.com>
2024-02-27 09:53:54 +0900

    fix Image css

--- src/app/Illustration/illust.tsx
+++ src/app/Illustration/illust.tsx
@@ -1,12 +1,19 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Illust = () => {
   return (
-    <div className='mt-[15%] flex justify-center'>
-      <Image src='/Illustrations/fydnr_v.jpeg' alt='illust1' width={500} height={500} className='w-[150px] h-[400px] object-cover'/>
+    <div className="mt-[15%] flex justify-center ">
+      <div className='w-[150px] h-[400px] relative'>
+        <Image
+          src="/Illustrations/fydnr_v.jpeg"
+          fill
+          alt="illust1"
+          className="object-cover"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Illust
+export default Illust;
