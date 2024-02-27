d7e44e42bc32e69aba8851d8f0aeabdb4d0da3e3
VOWAC <yukihakanai.2@gmail.com>
2024-02-27 10:04:59 +0900

    add bg animation

--- src/app/Illustration/illust.tsx
+++ src/app/Illustration/illust.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 
 const Illust = () => {
   return (
-    <div className="mt-[15%] flex justify-center ">
+    <div className="mt-[17%] flex justify-center group relative">
+      <div className="absolute inset-0 bg-cover bg-no-repeat z-0 transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-20 -top-48" style={{backgroundImage: 'url("/Illustrations/fydnr_v.jpeg")'}}></div>
       <div className="relative h-[300px] w-[150px] hover:w-[300px] transition-all duration-300 ease-in-out">
         <Image
           src="/Illustrations/fydnr_v.jpeg"
