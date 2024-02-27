024fa03a1eb17f2e5a906211246baff71c0858a1
VOWAC <yukihakanai.2@gmail.com>
2024-02-27 10:11:47 +0900

    fix bg animation

--- src/app/Illustration/illust.tsx
+++ src/app/Illustration/illust.tsx
@@ -1,18 +1,32 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
+import { useState } from 'react';
 
 const Illust = () => {
+  const [hover, setHover] = useState(false);
   return (
     <div className="mt-[17%] flex justify-center group relative">
-      <div className="absolute inset-0 bg-cover bg-no-repeat z-0 transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-20 -top-48" style={{backgroundImage: 'url("/Illustrations/fydnr_v.jpeg")'}}></div>
-      <div className="relative h-[300px] w-[150px] hover:w-[300px] transition-all duration-300 ease-in-out">
-        <Image
-          src="/Illustrations/fydnr_v.jpeg"
-          fill
-          alt="illust1"
-          className="object-cover"
-        />
+      <div
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className="relative h-[300px] w-[150px] hover:w-[300px] transition-all duration-300 ease-in-out">
+          <Image
+            src="/Illustrations/fydnr_v.jpeg"
+            fill
+            alt="illust1"
+            className="object-cover"
+          />
+        </div>
       </div>
+      {hover && (
+        <div
+          className="fixed inset-0 bg-cover bg-no-repeat z-[-1] opacity-20"
+          style={{ backgroundImage: 'url("/Illustrations/fydnr_v.jpeg")' }}
+        ></div>
+      )}
     </div>
   );
 };
