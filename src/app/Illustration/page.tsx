1cdd37dd4f1065e5e17594b16822174d8d8ae156
VOWAC <yukihakanai.2@gmail.com>
2024-02-27 15:42:22 +0900

    add images

--- src/app/Illustration/page.tsx
+++ src/app/Illustration/page.tsx
@@ -3,18 +3,24 @@ import Illust from './illust';
 
 const Page = () => {
   const imagePaths = [
-    '/Illustrations/fydnr_v.jpeg',
+    '/Illustrations/konoeda.jpg',
     '/Illustrations/fydnr_spider.jpg',
     '/Illustrations/vowac.jpg',
+    '/Illustrations/fydnr_v.jpeg',
+    '/Illustrations/cha_moon.jpg',
+    '/Illustrations/fydnr_fyh.jpg',
+    '/Illustrations/fydnr_hpb.jpg',
+    '/Illustrations/oritatamigasa.jpg',
   ];
 
   return (
     <div>
+      
       <div className="border-[1px] border-white w-full fixed top-1/2 -z-50" />
       <div className="flex justify-center items-center h-screen group relative space-x-8">
-        <Illust src={imagePaths[0]} />
-        <Illust src={imagePaths[1]} />
-        <Illust src={imagePaths[2]} />
+        {imagePaths.map((path, i) => (
+          <Illust key={i} src={path} />
+        ))}
       </div>
     </div>
   );
