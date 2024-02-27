5336f824429daa32952864d9009654bcbee3a907
VOWAC <yukihakanai.2@gmail.com>
2024-02-27 10:39:08 +0900

    added dynamically changing images

--- src/app/Illustration/illust.tsx
+++ src/app/Illustration/illust.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image';
 import React from 'react';
 import { useState } from 'react';
 
-const Illust = () => {
+type Props = {
+  src: string;
+};
+
+const Illust = ({ src }: Props) => {
   const [hover, setHover] = useState(false);
   return (
     <div className="flex justify-center items-center h-screen group relative">
@@ -13,17 +17,12 @@ const Illust = () => {
         onMouseLeave={() => setHover(false)}
       >
         <div className="relative h-[300px] w-[150px] hover:w-[300px] transition-all duration-300 ease-in-out">
-          <Image
-            src="/Illustrations/fydnr_v.jpeg"
-            fill
-            alt="illust1"
-            className="object-cover"
-          />
+          <Image src={src} fill alt="illust1" className="object-cover" />
         </div>
       </div>
       <div
         className={`fixed inset-0 z-[-1] bg-no-repeat bg-center bg-cover transition-opacity duration-300 ease-in-out ${hover ? 'opacity-20' : 'opacity-0'}`}
-        style={{ backgroundImage: 'url("/Illustrations/fydnr_v.jpeg")' }}
+        style={{ backgroundImage: `url(${src})` }}
       ></div>
     </div>
   );
--- src/app/Illustration/page.tsx
+++ src/app/Illustration/page.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import Illust from './illust'
 
 const Page = () => {
+  const imagePaths = [
+    '/Illustrations/fydnr_v.jpeg',
+    '/Illustrations/fydnr_spider.jpg',
+    '/Illustrations/vowac.jpg',
+  ]
+
   return (
     <div>
       <div className='border-[1px] border-white w-full fixed top-1/2 -z-50' />
-      <Illust />
+      <Illust src={imagePaths[2]}/>
     </div>
   )
 }
