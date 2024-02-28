59c680d13143daa5001ce87c0edc51e8f279101e
VOWAC <yukihakanai.2@gmail.com>
2024-02-28 09:39:44 +0900

    add header link

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-5 border-b-[1px] border-white transition-colors duration-300 text-2xl top-0 left-0 w-full z-50 fixed backdrop-blur-sm hover:bg-white hover:bg-opacity-10">
-      冬隣創造処
+    <div className="flex space-x-9 items-center p-5 border-b-[1px] border-white transition-colors duration-300 text-2xl top-0 left-0 w-full z-50 fixed backdrop-blur-sm hover:bg-white hover:bg-opacity-10">
+      <Link href="/">冬隣創造処</Link>
+      <nav>
+        <ul className="flex space-x-5 text-base">
+          <li>
+            <Link href="/about">自己紹介</Link>
+          </li>
+          <li>
+            <Link href="/Illustration">イラスト</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
