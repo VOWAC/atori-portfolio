00c791a656e938cc958d67becc2b0129e8ce29be
VOWAC <yukihakanai.2@gmail.com>
2024-02-28 10:48:12 +0900

    add top image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main>
+      <div>
+        <div className="border-[1px] border-white w-full fixed top-1/2 -z-50" />
+        <div className='flex justify-center mt-8'>
+          {/* 仮の画像 */}
+          <Image src="/dona.png" width={1200} height={1200} alt="profile" />
+        </div>
+      </div>
     </main>
   );
 }
